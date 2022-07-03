3d1a8c995dd7913b857a82b80e132a560df6da52
B-Oz <bettysoftwareng@gmail.com>
2022-07-03 20:36:37 +0300

    RandomUser

---
+++ RandomUser/src/App.js
@@ -0,0 +1,53 @@
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./components/Card.js";
+import "./App.scss";
+
+const url = "https://randomuser.me/api/";
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState([]);
+
+  const users = async () => {
+    // const response = await axios.get(url);
+    // const {data} = response;
+    try {
+      const { data } = await axios.get(url);
+      setUser(data.results[0]);
+      setLoading(false)
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // const users = async () =>{
+  //   const response = await fetch(url)
+  //   console.log(response);
+  //   const person = await response.json()
+  //   console.log(person);
+  //   setUser(person.results[0])
+  // }
+
+  useEffect(() => {
+    users();
+  }, []);
+
+  if(loading){
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <>
+
+    <div className="App">
+      <Card user={user}/>
+      {/* <h2>{user.email}</h2> */}
+    </div>
+      <button className="button" onClick={() => users()}>Random User</button>
+    </>
+  );
+}
+
+export default App;
---
+++ RandomUser/src/components/Card.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Card.scss";
+import phone from '../assets/phone.svg';
+import emails from '../assets/email.svg';
+import location from '../assets/location.svg';
+const Card = ({user}) => {
+  const {
+    picture: {medium},
+    name: {title, first, last},
+    email,
+    cell,
+    location: {city,country},
+    dob: {age},
+    registered: {date}
+  } = user;
+  return (
+    <div className='content'>
+      <div className='image'>
+        <img src={medium} alt="img" />
+      </div>
+      <h3>{title}   {first} {last}</h3>
+      <div className='svg'>
+       <img src={emails} alt="svg" />
+      </div>
+      <h4>{email}</h4>
+      <div className='svg'>
+       <img src={phone} alt="svg" />
+      </div>
+      <h4>{cell}</h4>
+      <div className='svg'>
+       <img src={location} alt="svg" />
+      </div>
+      <h4>{city}   {country}</h4>
+      <h4 className='age'>Age: {age}</h4>
+      <h4 className='regdate'>Register Date: {date.substring(0,10)}</h4>
+    </div>
+  );
+};
+export default Card
